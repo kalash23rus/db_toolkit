847edebc97b04cda5b95ee83deef2a4db6f9eca4
Chupray <kalashnikov.aa@phystech.edu>
2023-01-25 15:46:49 +0400

    Update postgres.py

--- db_tools/postgres.py
+++ db_tools/postgres.py
@@ -176,3 +176,40 @@ def array_generator4sql(items: list):
         items.append(items[0])
         items = tuple(items)
     return items
+
+def connect(params_dic):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params_dic)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        sys.exit(1) 
+    print("Connection successful")
+    return conn
+# conn = connect(param_dic)
+
+def execute_values(param_dic, df, table):
+    """
+    Using psycopg2.extras.execute_values() to insert the dataframe
+    """
+    conn = connect(param_dic)
+    # Create a list of tupples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ','.join(list(df.columns))
+    # SQL quert to execute
+    query  = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(cursor, query, tuples)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("execute_values() done")
+    cursor.close()
