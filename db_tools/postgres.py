a22ba99c2d8c3a1ad9c6b7623bcd12439df0a429
kalash23rus <kalash23rusa@gmail.com>
2023-05-30 18:39:48 +0400

    refactor postgresql code

--- db_tools/postgres.py
+++ db_tools/postgres.py
@@ -2,14 +2,17 @@ import psycopg2
 import pandas as pd
 from io import StringIO
 import json
-from copy import deepcopy
-import os
-import numpy as np
 import psycopg2.extras as extras
 
 __version__ = 'dev'
 
-def array_generator4sql(items: list):
+def array_generator4sql(items: list) -> tuple:
+    """
+    Convert a list of items into a tuple for SQL query.
+
+    :param items: list of items
+    :return: tuple of items
+    """
     if len(items) > 1:
         items = tuple(items)
     else:
@@ -17,7 +20,13 @@ def array_generator4sql(items: list):
         items = tuple(items)
     return items
 
-def create_connection(param_dic):
+def create_connection(param_dic: dict) -> psycopg2.extensions.connection:
+    """
+    Create a connection to the PostgreSQL database.
+
+    :param param_dic: dictionary containing connection parameters
+    :return: connection object
+    """
     conn = psycopg2.connect(
         host=param_dic["host"],
         database=param_dic["database"],
@@ -26,28 +35,32 @@ def create_connection(param_dic):
         port=param_dic["port"])
     return conn
 
-def execute_sql(query,param_dic):
-    conn = create_connection(param_dic)
-    cur = conn.cursor()
-    cur.execute(query)
-    cur.close()
-    conn.close()
-    return "Come to Kazahstan, It is nice"
+def select_to_df(query: str, param_dic: dict) -> pd.DataFrame:
+    """
+    Execute a SELECT query and return the result as a DataFrame.
 
-def select_to_df(query,param_dic):
+    :param query: SQL SELECT query
+    :param param_dic: dictionary containing connection parameters
+    :return: DataFrame containing query result
+    """
     conn = create_connection(param_dic)
     cur = conn.cursor()
     cur.execute(query)
     rows = cur.fetchall()
     columns = [description[0] for description in cur.description]
-    df=pd.DataFrame(rows,columns=columns)
+    df = pd.DataFrame(rows, columns=columns)
     cur.close()
     conn.close()
     return df
 
-def insert_values_to_table(
-                           table_name,
-                           df,param_dic):
+def insert_values_to_table(table_name: str, df: pd.DataFrame, param_dic: dict) -> None:
+    """
+    Insert values from a DataFrame into a table.
+
+    :param table_name: name of the table to insert values into
+    :param df: DataFrame containing values to insert
+    :param param_dic: dictionary containing connection parameters
+    """
     inserted_attr_list = list(df.columns)
     records = list(df.itertuples(index=False, name=None))
     n_table_attributes = len(inserted_attr_list)
@@ -60,7 +73,7 @@ def insert_values_to_table(
     try:
         conn = create_connection(param_dic)
         cur = conn.cursor()
-        cur.executemany(SQL_command,records)
+        cur.executemany(SQL_command, records)
         conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -69,12 +82,15 @@ def insert_values_to_table(
         if conn is not None:
             conn.close()
 
-def copy_from_stringio(param_dic, df, table):
+def copy_from_stringio(param_dic: dict, df: pd.DataFrame, table: str) -> int:
     """
-    Here we are going save the dataframe in memory 
-    and use copy_from() to copy it to the table
+    Copy data from a DataFrame to a table using StringIO.
+
+    :param param_dic: dictionary containing connection parameters
+    :param df: DataFrame containing data to copy
+    :param table: name of the table to copy data to
+    :return: 1 if an error occurred, otherwise None
     """
-    # save dataframe to an in memory buffer
     buffer = StringIO()
     df.to_csv(buffer, index_label='id', header=False)
     buffer.seek(0)
@@ -92,41 +108,33 @@ def copy_from_stringio(param_dic, df, table):
     print("copy_from_stringio() done")
     cursor.close()
 
-def insert_dictionary_into_postgres(dict_obj, table_name, jsonb_column_name,param_dic):
-    # Connect to the PostgreSQL database
-    conn = psycopg2.connect(
-        host=param_dic["host"],
-        database=param_dic["database"],
-        user=param_dic["user"],
-        password=param_dic["password"],
-        port=param_dic["port"]
-    )
+def insert_dictionary_into_postgres(dict_obj: dict, table_name: str, jsonb_column_name: str, param_dic: dict) -> None:
+    """
+    Insert a dictionary as a JSON object into a PostgreSQL table.
 
-    # Open a cursor to perform database operations
+    :param dict_obj: dictionary to insert
+    :param table_name: name of the table to insert the dictionary into
+    :param jsonb_column_name: name of the JSONB column to insert the dictionary into
+    :param param_dic: dictionary containing connection parameters
+    """
+    conn = create_connection(param_dic)
     cur = conn.cursor()
-
-    # Generate a query string for inserting the JSON object
     query = f"INSERT INTO {table_name} ({jsonb_column_name}) VALUES (%s)"
-
-    # Convert the dictionary to a JSON string
     json_str = json.dumps(dict_obj)
-
-    # Execute the query with the JSON string as a parameter
     cur.execute(query, (json_str,))
-
-    # Commit the transaction
     conn.commit()
-
-    # Close the cursor and the connection
     cur.close()
     conn.close()
-    
 
-def connect(params_dic):
-    """ Connect to the PostgreSQL database server """
+def connect(params_dic: dict) -> psycopg2.extensions.connection:
+    """
+    Connect to the PostgreSQL database server.
+
+    :param params_dic: dictionary containing connection parameters
+    :return: connection object
+    """
     conn = None
     try:
-        # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params_dic)
     except (Exception, psycopg2.DatabaseError) as error:
@@ -134,18 +142,19 @@ def connect(params_dic):
         sys.exit(1) 
     print("Connection successful")
     return conn
-# conn = connect(param_dic)
 
-def execute_values(param_dic, df, table):
+def execute_values(param_dic: dict, df: pd.DataFrame, table: str) -> int:
     """
-    Using psycopg2.extras.execute_values() to insert the dataframe
+    Insert values from a DataFrame into a table using psycopg2.extras.execute_values().
+
+    :param param_dic: dictionary containing connection parameters
+    :param df: DataFrame containing values to insert
+    :param table: name of the table to insert values into
+    :return: 1 if an error occurred, otherwise None
     """
     conn = connect(param_dic)
-    # Create a list of tupples from the dataframe values
     tuples = [tuple(x) for x in df.to_numpy()]
-    # Comma-separated dataframe columns
     cols = ','.join(list(df.columns))
-    # SQL quert to execute
     query  = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
     cursor = conn.cursor()
     try:
