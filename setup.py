bb13a7bcc3a256f32b6bcbabca07f8dd932ff92d
AlexDarkVape <kalash23rusa@gmail.com>
2023-09-07 18:07:59 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
-from db_tools import __version__
-
 # Read the contents of the requirements.txt file
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+# Read the version directly from the __init__.py file
+with open('db_tools/__init__.py') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip("'")
+            break
+
 setup(
     name='db_tools',
-    version=__version__,
-
+    version=version,
     url='https://gitlab.com/Kalashnikov_A/kalashnikov_toolkit/-/blob/sandbox',
     author='kalash_23rus',
     author_email='a.kalashnikov@insilicomedicine.com',
-
     packages=find_packages(),
     install_requires=requirements,
 )
